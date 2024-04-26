2a3c6dc2edc17dfeb6c84272afffacd50cbf5719
Andrii Popov <whatyalookinat23@gmail.com>
2024-04-26 22:29:51 +0300

    fixed: importing mongoose

--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
-import mongoose, { mongo } from "mongoose";
+import { Schema, model } from "mongoose";
 
-const UserSchema = new mongoose.Schema({
+const UserSchema = new Schema({
     fullName: {
         type: String,
         require: true,
@@ -22,4 +22,4 @@ const UserSchema = new mongoose.Schema({
 },
 );
 
-export default mongoose.model("User", UserSchema);
+export default model("User", UserSchema);
