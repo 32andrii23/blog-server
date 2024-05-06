aa01773eb5fdbc7669e2e9826a78c9997245cc25
Andrii Popov <whatyalookinat23@gmail.com>
2024-05-06 19:01:40 +0300

    added: auth middleware

--- routers/auth.js
+++ routers/auth.js
@@ -2,6 +2,7 @@ import Router from "express";
 import { body } from "express-validator";
 
 import userController from "../controllers/user-controller.js";
+import authMiddleware from "../middleware/auth-middleware.js";
 
 const router = new Router();
 
@@ -17,6 +18,10 @@ router.post("/logout", userController.logout);
 router.get("/activate/:link", userController.activate);
 router.get("/refresh", userController.refresh);
 
-router.get("/users", userController.getUsers);
+router.get(
+    "/users", 
+    authMiddleware, 
+    userController.getUsers
+);
 
 export default router;
